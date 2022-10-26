35f14152d6773400c677969b3ca935e9c66b8462
2qx <2qx.git@small.neomailbox.ch>
2022-10-26 14:26:16 -0400

    bump annuity test fee

--- packages/phi/src/contract/annuity/Annuity.test.ts
+++ packages/phi/src/contract/annuity/Annuity.test.ts
@@ -82,7 +82,7 @@ describe(`Annuity Class Tests`, () => {
             });
             await p1.execute(charlie.getDepositAddress())
         }
-        expect(await charlie.getBalance("sat")).toBeGreaterThan(13900)
+        expect(await charlie.getBalance("sat")).toBeGreaterThan(13800)
         expect(await bob.getBalance("sat")).toBe(50000)
         expect(p1.isTestnet()).toEqual(true)
         expect(await p1.getBalance()).toBeGreaterThan(900000)
