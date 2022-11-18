ea7e449c146bf4fd86734b1f55c5e2522413ed96
2qx <2qx.git@small.neomailbox.ch>
2022-11-18 11:01:55 -0500

    bump social header

---
+++ docs/_app/immutable/chunks/0-5c0015a7.js
@@ -0,0 +1,2 @@
+import{_ as r}from"./_layout-e72c0abe.js";import{default as t}from"../components/pages/_layout.svelte-4e7a5b09.js";export{t as component,r as shared};
+//# sourceMappingURL=0-5c0015a7.js.map
--- docs/_app/immutable/chunks/1-28b5e40b.js
+++
@@ -1,2 +0,0 @@
-import{default as t}from"../components/error.svelte-1df8b25c.js";export{t as component};
-//# sourceMappingURL=1-28b5e40b.js.map
