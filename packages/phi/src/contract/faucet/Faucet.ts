2bb8e260791fabc1199bbce0284edefa9174c4fc
2qx <2qx.git@small.neomailbox.ch>
2023-09-24 13:18:34 -0500

    appify

--- packages/phi/src/contract/faucet/Faucet.ts
+++ packages/phi/src/contract/faucet/Faucet.ts
@@ -21,6 +21,7 @@ export class Faucet extends BaseUtxPhiContract implements UtxPhiIface {
     let script: Artifact;
     if (options.version === 2) {
       script = v2;
+      if (payout < Faucet.minPayout) throw Error("Payout below dust threshold");
     } else if (options.version === 1) {
       script = v1;
     } else if (options.version === 0) {
@@ -29,7 +30,6 @@ export class Faucet extends BaseUtxPhiContract implements UtxPhiIface {
       throw Error("Unrecognized Faucet Version");
     }
 
-    if (payout < Faucet.minPayout) throw Error("Payout below dust threshold");
 
     super(options.network!, script, [BigInt(period), BigInt(payout), BigInt(index)]);
     this.options = options;
