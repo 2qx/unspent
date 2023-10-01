9c2ad11a1086d93bcac6b5278da652575c190deb
2qx <2qx.git@small.neomailbox.ch>
2023-10-01 15:56:10 -0500

    fix bug with search by prefix cutoff

--- packages/phi/src/contract/perpetuity/Perpetuity.ts
+++ packages/phi/src/contract/perpetuity/Perpetuity.ts
@@ -335,6 +335,7 @@ export class Perpetuity extends BaseUtxPhiContract implements UtxPhiIface {
     // Filter to inputs of sufficient age
     if (!utxos) utxos = await this.getUtxos(Number(this.period));
 
+    
     // If the contract is version 2 or higher, restrict to one input.
     if (utxos) {
       if (this.options!.version! >= 2 && utxos!.length > 1) utxos = utxos.slice(-1)
