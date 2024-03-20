614518aa4f7ba5cad3da47e43befde0ae599bfbf
2qx <2qx.git@small.neomailbox.ch>
2024-03-20 13:58:52 -0500

    fix app footer

--- packages/cat/sql/tvl.sql
+++ packages/cat/sql/tvl.sql
@@ -1,8 +1,9 @@
-SELECT 
+SELECT * FROM  (SELECT 
   Date(ts.timestamp) "day", 
   count(ts.locking_bytecode) "count_opened_cummulative",
   round(sum(ts.value),6) "tlv",
   round(sum(ts.value*f.value),2) "usd"
 from mainnet_series ts 
 left join mainnet_fiat f on Date(f.timestamp) = Date(ts.timestamp)
-group by ts.timestamp order by ts.timestamp
+group by ts.timestamp order by ts.timestamp) a 
+where usd is not null
