2c29976eaaf1c44c20a1850927dea59ca77f4b61
2qx <2qx.git@small.neomailbox.ch>
2024-03-31 04:58:22 -0500

    hausa and stats

--- packages/cat/src/query.ts
+++ packages/cat/src/query.ts
@@ -191,7 +191,7 @@ export async function getPriceHistory() {
   const response = await axios.get(COINGECKO_CHART, {
     params: {
       vs_currency: "usd",
-      from: "1501546841",
+      from: "1682930834",
       to: Date.now() / 1000
 
     }
