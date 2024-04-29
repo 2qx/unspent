d1c4a7fd478de46f88222e82ea178ecf891074f5
2qx <2qx.git@small.neomailbox.ch>
2024-04-29 12:13:01 -0500

    fix reset bug

--- packages/chi/src/routes/chart/+page.svelte
+++ packages/chi/src/routes/chart/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { afterUpdate } from 'svelte';
 	import { _, isLoading } from 'svelte-i18n';
 	import { receiptAddressStore, stateStore } from '$lib/store.js';
 	import ContractChartSection from '$lib/ContractChartSection.svelte';
@@ -12,23 +13,26 @@
 		receiptAddress = value;
 	});
 
-	stateStore.subscribe((value) => {
-		stateValue = Number(value);
-		if (stateValue < 5) {
-			stateStore.set('5');
-		}
+	afterUpdate(() => {
+		console.log("chart seen")
+		stateStore.subscribe((value) => {
+			stateValue = Number(value);
+			if (stateValue < 5) {
+				stateStore.set('5');
+			}
+		});
 	});
 </script>
 
 {#if $isLoading}
 	...
 {:else if !receiptAddress}
-<h1>
-	{$_('8')}
-</h1>
-<a href="/">
-	<img width="100%" src="/h/09.svg" alt={$_('8')} />
-</a>
+	<h1>
+		{$_('8')}
+	</h1>
+	<a href="/">
+		<img width="100%" src="/h/09.svg" alt={$_('8')} />
+	</a>
 {:else}
 	<ContractChartSection {receiptAddress} />
 {/if}
